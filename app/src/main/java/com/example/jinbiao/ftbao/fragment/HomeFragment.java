9bf26c84edbc4965ee2f56daa933592bc942dfb4
root <820728252@qq.com>
2017-06-07 22:12:28 +0800

    完成轮播图

--- app/src/main/java/com/example/jinbiao/ftbao/fragment/HomeFragment.java
+++ app/src/main/java/com/example/jinbiao/ftbao/fragment/HomeFragment.java
@@ -6,25 +6,37 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.jinbiao.ftbao.MainActivity;
 import com.example.jinbiao.ftbao.R;
 import com.example.jinbiao.ftbao.base.BaseFragment;
+import com.example.jinbiao.ftbao.bean.ADInfo;
+import com.example.jinbiao.ftbao.pager.cycleviewpager.CycleViewPager;
+import com.example.jinbiao.ftbao.utils.ViewFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class HomeFragment extends BaseFragment {
-    @BindView(R.id.home_vp)
-    ViewPager vPager;
 
+    private List<ImageView> views = new ArrayList<ImageView>();
+    private List<ADInfo> infos = new ArrayList<ADInfo>();
+    private CycleViewPager cycleViewPager;
+
+    private String[] imageUrls = {"http://img.taodiantong.cn/v55183/infoimg/2013-07/130720115322ky.jpg",
+            "http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg",
+            "http://pic18.nipic.com/20111215/577405_080531548148_2.jpg",
+            "http://pic15.nipic.com/20110722/2912365_092519919000_2.jpg",
+            "http://pic.58pic.com/58pic/12/64/27/55U58PICrdX.jpg"};
     @Override
     public HomeFragment newInstance(){
         return new HomeFragment();
-    }
-
-    @Override
-    protected void initView(View view) {
     }
 
     @Nullable
@@ -36,4 +48,53 @@ public class HomeFragment extends BaseFragment {
         initView(view);
         return view;
     }
+
+    @Override
+    protected void initView(View view) {
+        cycleViewPager = (CycleViewPager) getActivity().getFragmentManager().findFragmentById(R.id.fragment_cycle_viewpager_content);
+        cycleViewPagerOn(cycleViewPager);
+    }
+
+    private void cycleViewPagerOn(CycleViewPager cycleViewPager){
+        for(int i = 0; i < imageUrls.length; i ++){
+            ADInfo info = new ADInfo();
+            info.setUrl(imageUrls[i]);
+            info.setContent("图片-->" + i );
+            infos.add(info);
+        }
+
+        // 将最后一个ImageView添加进来
+        views.add(ViewFactory.getImageView(getActivity(), infos.get(infos.size() - 1).getUrl()));
+        for (int i = 0; i < infos.size(); i++) {
+            views.add(ViewFactory.getImageView(getActivity(), infos.get(i).getUrl()));
+        }
+        // 将第一个ImageView添加进来
+        views.add(ViewFactory.getImageView(getActivity(), infos.get(0).getUrl()));
+
+        // 设置循环，在调用setData方法前调用
+        cycleViewPager.setCycle(true);
+
+        // 在加载数据前设置是否循环
+        cycleViewPager.setData(views, infos, mAdCycleViewListener);
+        //设置轮播
+        cycleViewPager.setWheel(true);
+
+        // 设置轮播时间，默认5000ms
+        cycleViewPager.setTime(3000);
+        //设置圆点指示图标组居中显示，默认靠右
+        cycleViewPager.setIndicatorCenter();
+    }
+
+    private CycleViewPager.ImageCycleViewListener mAdCycleViewListener = new CycleViewPager.ImageCycleViewListener() {
+
+        @Override
+        public void onImageClick(ADInfo info, int position, View imageView) {
+            if (cycleViewPager.isCycle()) {
+                position = position - 1;
+                Toast.makeText(getActivity(),
+                        "position-->" + info.getContent(), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    };
 }
