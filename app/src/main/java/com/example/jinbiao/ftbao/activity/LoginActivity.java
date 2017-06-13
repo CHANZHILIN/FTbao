f349a7d094071fd50708e8519fd8fbfac3b47093
jiabao <1194899285@qq.com>
2017-06-13 20:42:57 +0800

    全新登陆界面

--- app/src/main/java/com/example/jinbiao/ftbao/activity/LoginActivity.java
+++ app/src/main/java/com/example/jinbiao/ftbao/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.jinbiao.ftbao.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,14 +31,15 @@ public class LoginActivity extends AppCompatActivity {
     EditText etAccount;
     @BindView(R.id.et_password)
     EditText etPassword;
-    @BindView(R.id.btn_eye)
-    Button btnEye;
+
     @BindView(R.id.btn_login)
     Button btnLogin;
     @BindView(R.id.tv_resister)
     TextView tvResister;
     @BindView(R.id.tv_findpassword)
     TextView tvFindpassword;
+    @BindView(R.id.toolbar_login)
+    Toolbar toolbarLogin;
 
     private OkHttpClient client;
 
@@ -46,23 +48,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbarLogin);
     }
 
-    @OnClick({R.id.et_account, R.id.et_password, R.id.btn_eye, R.id.btn_login, R.id.tv_resister, R.id.tv_findpassword})
+    @OnClick({R.id.et_account, R.id.et_password, R.id.btn_login, R.id.tv_resister, R.id.tv_findpassword})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.et_account:
                 break;
             case R.id.et_password:
                 break;
-            case R.id.btn_eye:
-                break;
             case R.id.btn_login:
-                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 break;
             case R.id.tv_resister:
-                Intent intent=new Intent();
-                intent.setClass(this,RegisterActivity.class);
+                Intent intent = new Intent();
+                intent.setClass(this, RegisterActivity.class);
                 startActivity(intent);
 
                 break;
@@ -71,12 +72,12 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void postRequest(String username,String password){
-        client =new OkHttpClient();
+    private void postRequest(String username, String password) {
+        client = new OkHttpClient();
         //建立请求表单，添加发送到服务器的数据
         RequestBody formBody = new FormBody.Builder()
-                .add("username",username)
-                .add("password",password)
+                .add("username", username)
+                .add("password", password)
                 .build();
         //发起请求
         Request request = new Request.Builder()
