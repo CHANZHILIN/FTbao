578c477d85cda5adf69eac2e5d12fa8dc523a126
wujinbiao <820728252@qq.com>
2017-06-13 22:32:33 +0800

    首页Tshirt表增加了details字段，把数据库更新就可以了

--- app/src/main/java/com/example/jinbiao/ftbao/bean/TshirtData.java
+++ app/src/main/java/com/example/jinbiao/ftbao/bean/TshirtData.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class TshirtData {
 
     /**
-     * data : [{"imgurl":"http://d9.yihaodianimg.com/N07/M03/4B/2A/ChEbvFU2Cu2AHKWyAAGORN4XvAA00800_230*322.jpg","goodcomment":"95%","commentcount":"20","proinfo":"Semir/森马 2015夏装新款短袖t恤男休闲圆领字母星空t恤衫打底衫男装潮1y0214001301(中花灰0017)","price":"￥19.9","storename":"森马官方旗舰店","id":10,"proact":"6.11-6.19专区满199立减100"},{"imgurl":"http://d7.yihaodianimg.com/N10/M09/1D/13/ChEi2lkegAyAaJASAAbh5wy4OJI78200_230*322.jpg","goodcomment":"100%","commentcount":"101","proinfo":"广告 Happy time/幸福时光 T恤男 2017夏季新款男士T恤潮流韩版修身T恤男","price":"￥68.0","storename":"京帅服饰专营店","id":11,"proact":""},{"imgurl":"http://d8.yihaodianimg.com/N09/M02/7B/1E/ChEi2FjuSGyAa4ikAAdZrTjUJOk70700_230*322.jpg","goodcomment":"100%","commentcount":"70","proinfo":"圣晟 短袖T恤POLO衫2017夏季新品男士休闲吸汗舒适翻领大码t恤潮流拼色直筒打底衫(藏青色)","price":"￥89","storename":"圣晟旗舰店","id":12,"proact":""},{"imgurl":"http://d9.yihaodianimg.com/N09/M00/66/F5/ChEi11ZgAsOAcHI8AAFeQhEe01420600_230*322.jpg","goodcomment":"98%","commentcount":"50","proinfo":"Jeanswest/真维斯 夏装 男装立领条纹牛角短袖T恤JW-53-173507(2065 淡灰)","price":"￥90","storename":"真维斯官方旗舰店","id":13,"proact":""},{"imgurl":"http://d8.yihaodianimg.com/N10/M02/65/AF/ChEi3FcZuZyACBOBAAZIerfcdEI97000_230*322.jpg","goodcomment":"100%","commentcount":"5","proinfo":"美特斯邦威 美国队长系列夏装新款男MEE漫威系列前整片印花短袖恤(黑色组)","price":"￥59","storename":"美特斯邦威官方旗舰店","id":14,"proact":"\u201c威\u201d力引爆夏天 满199减50"},{"imgurl":"http://d6.yihaodianimg.com/N09/M04/BE/0E/ChEi2FkJ2WaAVRVgAAWwDRPMt4s90700_230*322.jpg","goodcomment":"100%","commentcount":"64","proinfo":"寓语 2017夏装新品男士短袖t恤时尚迷彩青少年纯色圆领t恤修身时尚大码简约t恤(藏青色)","price":"￥89","storename":"寓语旗舰店","id":15,"proact":""},{"imgurl":"http://d8.yihaodianimg.com/N09/M04/B2/C5/ChEi2FkHHIuAKnmvAAb0dba1DEU46600_230*322.jpg","goodcomment":"98%","commentcount":"99","proinfo":"广告 Happy time/幸福时光 T恤男2017春季新款男士纯色t恤修身时尚休闲T恤男","price":"￥68.0","storename":"京帅服饰专营店","id":16,"proact":""},{"imgurl":"http://d6.yihaodianimg.com/N10/M08/00/2E/ChEi2lkP8W6AHy6hAAN4HCTmDas79700_230*322.jpg","goodcomment":"100%","commentcount":"2","proinfo":"Lee 【618提前购】【情侣款】夏季新款 简洁版LOGO装饰 男士圆领短袖T恤 时尚潮流女士棉质T恤(L250232LQK11黑男)","price":"￥149","storename":"LEE专卖店","id":17,"proact":"领券 满300减100 满600减200"},{"imgurl":"http://d9.yihaodianimg.com/V00/M0B/69/32/CgQDslUZ_XaABlt2AAPEP8yD9gE10400_230*322.jpg","goodcomment":"100%","commentcount":"2","proinfo":"HANRI'S瀚瑞 中老年男士夏季新款鸡心领短袖T恤透气棉套头衫印花运动休闲男装LML2096(深咖绿)","price":"￥79","storename":"瀚瑞服饰旗舰店","id":18,"proact":"年中盛典，3件8折！加15元换购！"},{"imgurl":"http://d8.yihaodianimg.com/N10/M04/D8/9C/ChEi21juS0GAbzfXAAV8zTW8dWM13100_230*322.jpg","goodcomment":"100%","commentcount":"114","proinfo":"圣晟 2017夏季新款男装简约短袖t恤针织圆领纯棉潮流海魂衫条纹青少年加大码T恤(白色)","price":"￥98","storename":"圣晟旗舰店","id":19,"proact":""}]
+     * data : [{"imgurl":"http://d9.yihaodianimg.com/N03/M02/6A/32/CgQCtFOif0SALflfAAL-UJ0kcBE84800_230*322.jpg","goodcomment":"95%","commentcount":"69","proinfo":"Semir/森马 夏装新款男士潮印花针织圆领短袖直筒t恤衫专柜款1y0214001025(湖蓝)","price":"￥29.9","storename":"森马官方旗舰店","details":"http://item.yhd.com/item/31604076","id":10,"proact":"领券满99减10元"},{"imgurl":"http://d6.yihaodianimg.com/N08/M05/1F/FE/ChEi1lkauR2AUsQ2AAKbGYuNb-o58200_230*322.jpg","goodcomment":"","commentcount":"25","proinfo":"广告 OFF WHITE 2017夏季新款时尚潮牌男装宽松休闲背部迷彩印花男士短袖T恤","price":"￥139.0","storename":"潮牌潮流服饰专营店","details":"http://tracker.yhd.com/ad-dolphin-go/go?v=Y2YWDer8KoSJvIDLTjzgLjuFqlnVoUvy0Nmhbsf3PYkUkewiPMYrOVtEshjvlmijuuGwe0wAcCsccVcDp-4pndmJkBLdfhYxH3gF-aio3NKISwXMpcsWfFKqFhi8EO1-9Vey6u1mE3io85ZIxlUVlzWhbk7qTTTJtC21x9bFweoP0smCPUBvT0KNnPAcBJWF2AwZmvPg7rpk2IEuTa20ZcK7YAYnATaQExYbCovKrzmPoObTyKJdYUhfxgBVrC2PTpwEvpgT5N9nc0YKzmq76q5QCvUeZXtgx1CNg9kHeObO46SI3K_0DpSzxkEHAGU-","id":11,"proact":""},{"imgurl":"http://d8.yihaodianimg.com/N08/M07/D3/8B/ChEi1VjtkaGAOgzcAAOazMJBb8g62100_230*322.jpg","goodcomment":"91%","commentcount":"12","proinfo":"Jeanswest/真维斯 夏装 男装休闲舒适圆领胸间短袖T恤JW-62-173579(2100 漂白色)","price":"￥35.9","storename":"真维斯官方旗舰店","details":"http://item.yhd.com/item/63310776","id":12,"proact":""},{"imgurl":"http://d8.yihaodianimg.com/N09/M02/7B/1E/ChEi2FjuSGyAa4ikAAdZrTjUJOk70700_230*322.jpg","goodcomment":"100%","commentcount":"70","proinfo":"圣晟 短袖T恤POLO衫2017夏季新品男士休闲吸汗舒适翻领大码t恤潮流拼色直筒打底衫(藏青色)","price":"￥89","storename":"圣晟旗舰店","details":"http://item.yhd.com/item/62017497","id":13,"proact":""},{"imgurl":"http://d6.yihaodianimg.com/N09/M04/BE/0E/ChEi2FkJ2WaAVRVgAAWwDRPMt4s90700_230*322.jpg","goodcomment":"100%","commentcount":"64","proinfo":"寓语 2017夏装新品男士短袖t恤时尚迷彩青少年纯色圆领t恤修身时尚大码简约t恤(藏青色)","price":"￥89","storename":"寓语旗舰店","details":"http://item.yhd.com/item/62697182","id":14,"proact":""},{"imgurl":"http://d6.yihaodianimg.com/N10/M08/00/2E/ChEi2lkP8W6AHy6hAAN4HCTmDas79700_230*322.jpg","goodcomment":"100%","commentcount":"2","proinfo":"Lee 【618提前购】【情侣款】夏季新款 简洁版LOGO装饰 男士圆领短袖T恤 时尚潮流女士棉质T恤(L250232LQK11黑男)","price":"￥149","storename":"LEE专卖店","details":"http://item.yhd.com/item/74375611","id":15,"proact":"领券 满300减100 满600减200"},{"imgurl":"http://d8.yihaodianimg.com/N09/M09/A5/09/ChEi2FkABPKAKlT-AAHtOZbcTZQ17400_230*322.jpg","goodcomment":"100%","commentcount":"1","proinfo":"广告 Boy London 短袖T恤2017夏季新款时尚潮牌男装宽松休闲圆领字母数字印花男士短袖体恤男","price":"￥179.0","storename":"潮牌潮流服饰专营店","details":"http://tracker.yhd.com/ad-dolphin-go/go?v=Y2YWDer8KoSJvIDLTjzgLjuFqlnVoUvy0Nmhbsf3PYkUkewiPMYrOVtEshjvlmijuuGwe0wAcCsccVcDp-4pndmJkBLdfhYxH3gF-aio3NKISwXMpcsWfFKqFhi8EO1-9Vey6u1mE3io85ZIxlUVl5x1-j8zdQO-bywRtr3gsQO3YFlMfaG347VVqvClTbYo_sE4joC3Ek8T5HGYSivlPjLW9HGUKaNz3dZq-QtVRPGSDeqNzWtwAShtg_sJX9eHAeHiAM8QNpH9L9U0QMu9hQFsFvlFYj8C1N1CLChluS2TDEITwFafthaH9ak0ZV1F","id":16,"proact":""},{"imgurl":"http://d9.yihaodianimg.com/N10/M01/DB/57/ChEi3FjwNMyAUXsQAAM5pS_ayH809200_230*322.jpg","goodcomment":"90%","commentcount":"87","proinfo":"Jantour/簡徒 夏季新款韩版修身立领休闲短袖T恤套装男KW8031(白衣+黑裤)","price":"￥59","storename":"简徒官方旗舰店","details":"http://item.yhd.com/item/61003407","id":17,"proact":""},{"imgurl":"http://d6.yihaodianimg.com/N05/M01/6A/4D/CgQI0lcEwrSAGvXdAADL1SJivjc52900_230*322.jpg","goodcomment":"","commentcount":"25","proinfo":"伦敦雾 男士商务休闲短袖T恤衫LS13KT301(B1)","price":"￥349","storename":"恒烨服饰专营店","details":"http://item.yhd.com/item/61121827","id":18,"proact":""},{"imgurl":"http://d7.yihaodianimg.com/N09/M02/F8/C4/ChEi11kipKCAJVVrAAEx8oohAEM35400_230*322.jpg","goodcomment":"100%","commentcount":"27","proinfo":"花花公子 2017夏季新款男士短袖T恤v领纽扣装饰纯色韩版潮流半袖体恤青年大码打底上衣潮HYD-8742(深灰)","price":"￥139","storename":"简爱鞋服专营店","details":"http://item.yhd.com/item/74762245","id":19,"proact":""}]
      * message : success
      */
 
@@ -34,14 +34,15 @@ public class TshirtData {
 
     public static class Tshirt {
         /**
-         * imgurl : http://d9.yihaodianimg.com/N07/M03/4B/2A/ChEbvFU2Cu2AHKWyAAGORN4XvAA00800_230*322.jpg
+         * imgurl : http://d9.yihaodianimg.com/N03/M02/6A/32/CgQCtFOif0SALflfAAL-UJ0kcBE84800_230*322.jpg
          * goodcomment : 95%
-         * commentcount : 20
-         * proinfo : Semir/森马 2015夏装新款短袖t恤男休闲圆领字母星空t恤衫打底衫男装潮1y0214001301(中花灰0017)
-         * price : ￥19.9
+         * commentcount : 69
+         * proinfo : Semir/森马 夏装新款男士潮印花针织圆领短袖直筒t恤衫专柜款1y0214001025(湖蓝)
+         * price : ￥29.9
          * storename : 森马官方旗舰店
+         * details : http://item.yhd.com/item/31604076
          * id : 10
-         * proact : 6.11-6.19专区满199立减100
+         * proact : 领券满99减10元
          */
 
         private String imgurl;
@@ -50,6 +51,7 @@ public class TshirtData {
         private String proinfo;
         private String price;
         private String storename;
+        private String details;
         private int id;
         private String proact;
 
@@ -101,6 +103,14 @@ public class TshirtData {
             this.storename = storename;
         }
 
+        public String getDetails() {
+            return details;
+        }
+
+        public void setDetails(String details) {
+            this.details = details;
+        }
+
         public int getId() {
             return id;
         }
@@ -115,20 +125,6 @@ public class TshirtData {
 
         public void setProact(String proact) {
             this.proact = proact;
-        }
-
-        @Override
-        public String toString() {
-            return "Tshirt{" +
-                    "imgurl='" + imgurl + '\'' +
-                    ", goodcomment='" + goodcomment + '\'' +
-                    ", commentcount='" + commentcount + '\'' +
-                    ", proinfo='" + proinfo + '\'' +
-                    ", price='" + price + '\'' +
-                    ", storename='" + storename + '\'' +
-                    ", id=" + id +
-                    ", proact='" + proact + '\'' +
-                    '}';
         }
     }
 }
