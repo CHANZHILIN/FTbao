108d417cfb8679d05bf69e0251b1377c270c6394
wdmzszly <879810106@qq.com>
2017-06-13 09:24:42 +0800

    更新发现页面

--- app/src/main/java/com/example/jinbiao/ftbao/fragment/DiscoveryFragment.java
+++ app/src/main/java/com/example/jinbiao/ftbao/fragment/DiscoveryFragment.java
@@ -2,19 +2,36 @@ package com.example.jinbiao.ftbao.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.jinbiao.ftbao.R;
+import com.example.jinbiao.ftbao.adapter.DiscoveryAdapter;
 import com.example.jinbiao.ftbao.base.BaseFragment;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class DiscoveryFragment extends BaseFragment {
+
+    private DiscoveryAdapter mAdapter;
+    RecyclerView recyclerView;
+
+    private String[] imageUrls = {"http://img.taodiantong.cn/v55183/infoimg/2013-07/130720115322ky.jpg",
+            "http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg",
+            "http://pic18.nipic.com/20111215/577405_080531548148_2.jpg",
+            "http://pic15.nipic.com/20110722/2912365_092519919000_2.jpg",
+            "http://pic.58pic.com/58pic/12/64/27/55U58PICrdX.jpg"};
+    private String[] images = {"http://img.taodiantong.cn/v55183/infoimg/2013-07/130720115322ky.jpg",
+            "http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg",
+            "http://pic18.nipic.com/20111215/577405_080531548148_2.jpg",
+            "http://pic15.nipic.com/20110722/2912365_092519919000_2.jpg",
+            "http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg",
+            "http://pic18.nipic.com/20111215/577405_080531548148_2.jpg",
+            "http://pic15.nipic.com/20110722/2912365_092519919000_2.jpg",
+            "http://pic.58pic.com/58pic/12/64/27/55U58PICrdX.jpg"};
 
     @Override
     public DiscoveryFragment newInstance(){
@@ -29,10 +46,22 @@ public class DiscoveryFragment extends BaseFragment {
         //先绑定ButterKnife 要不然不能用
         ButterKnife.bind(this,view);
         initView(view);
+        initAdapter();
         return view;
     }
 
     @Override
     protected void initView(View view) {
+        recyclerView = (RecyclerView) view.findViewById(R.id.disco_recycler_view);
+
     }
+
+    private void initAdapter(){
+        mAdapter = new DiscoveryAdapter(getActivity(),images);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+
+
+
 }
