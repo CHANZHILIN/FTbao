c3efb31a1757ab8121bd08045a0f2283dbb53d44
Chan <1181785848@qq.com>
2017-06-15 11:12:30 +0800

    Merge branch 'master' of E:\Android_Studio\WorkPlace\FTbao with conflicts.

--- app/src/main/java/com/example/jinbiao/ftbao/adapter/CartReCycleViewAdapter.java
+++ app/src/main/java/com/example/jinbiao/ftbao/adapter/CartReCycleViewAdapter.java
@@ -1,25 +1,92 @@
 package com.example.jinbiao.ftbao.adapter;
 
+import android.app.FragmentManager;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jinbiao.ftbao.R;
+import com.example.jinbiao.ftbao.bean.CartData;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by CHEN_ on 2017/6/12.
  */
 
-public class CartReCycleViewAdapter extends RecyclerView.Adapter{
+public class CartReCycleViewAdapter extends RecyclerView.Adapter {
+
+
+
+    private List<CartData.DataBean> cartDatas;
+
+    private Context context;
+    private FragmentManager fragmentManager;
+    private LayoutInflater mLayoutInflater;
+
+
+    /**
+     * 添加数据
+     */
+    public void setCartDatas(List<CartData.DataBean> cartDatas) {
+        this.cartDatas.addAll(cartDatas);
+    }
+
+    public CartReCycleViewAdapter(Context context, FragmentManager fragmentManager, List<CartData.DataBean> list) {
+        this.context = context;
+       /* cartDatas = list;*/
+        this.fragmentManager = fragmentManager;
+        this.mLayoutInflater = LayoutInflater.from(context);
+        Toast.makeText(context, ""+cartDatas.get(0).getStorename(),Toast.LENGTH_LONG).show();
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cycleview_cart, parent, false);
+        cartViewHolder viewHolder = new cartViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        ((cartViewHolder)holder).mTvStorename.setText(cartDatas.get(position).getStorename());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return cartDatas==null ? 0 : cartDatas.size();
+    }
+
+
+    public static class cartViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.cb_storename)
+        CheckBox mCbStorename;
+        @BindView(R.id.cb_goods)
+        CheckBox mCbGoods;
+        @BindView(R.id.iv_goodsimg)
+        ImageView mIvGoodsimg;
+        @BindView(R.id.tv_storename)
+        TextView mTvStorename;
+        @BindView(R.id.tv_goodskind)
+        TextView mTvGoodskind;
+        @BindView(R.id.tv_goodsprice)
+        TextView mTvGoodsprice;
+        @BindView(R.id.tv_goodscount)
+        TextView mTvGoodscount;
+
+        public cartViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
     }
 }
--- app/src/main/java/com/example/jinbiao/ftbao/fragment/CartFragment.java
+++ app/src/main/java/com/example/jinbiao/ftbao/fragment/CartFragment.java
@@ -6,23 +6,44 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import com.example.jinbiao.ftbao.R;
 import com.example.jinbiao.ftbao.adapter.CartReCycleViewAdapter;
 import com.example.jinbiao.ftbao.base.BaseFragment;
+import com.example.jinbiao.ftbao.bean.CartData;
+import com.example.jinbiao.ftbao.interFace.ICart;
+import com.example.jinbiao.ftbao.utils.Constant;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class CartFragment extends BaseFragment {
+
+    private static final String TAG = "CartFragment";
 
     @BindView(R.id.recycler_view_cart)
     RecyclerView mRecyclerViewCart;
     private LinearLayoutManager mLinearLayoutManager;
     private FragmentManager mFragmentManager;
+    private CartReCycleViewAdapter mCartReCycleViewAdapter;
+
+    private List<CartData.DataBean> cartDatas;
+    public List<CartData.DataBean> cart;
 
     @Override
     public CartFragment newInstance() {
@@ -41,6 +62,8 @@ public class CartFragment extends BaseFragment {
         //先绑定ButterKnife 要不然不能用
         ButterKnife.bind(this, view);
         initView(view);
+        //获取购物车数据
+        getCartData();
         initAdapter();
         return view;
     }
@@ -50,12 +73,54 @@ public class CartFragment extends BaseFragment {
         //设置高度固定，提高性能
         mRecyclerViewCart.setHasFixedSize(true);
         mFragmentManager = this.getActivity().getFragmentManager();
+        mCartReCycleViewAdapter = new CartReCycleViewAdapter(getActivity(),mFragmentManager,cart);
         //设置适配器
-        mRecyclerViewCart.setAdapter(new CartReCycleViewAdapter());
+        mRecyclerViewCart.setAdapter(mCartReCycleViewAdapter);
 
     }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
     }
+
+    public void getCartData(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constant.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ICart carts = retrofit.create(ICart.class);
+        Call<CartData> call = carts.getCartData(2);
+        call.enqueue(new Callback<CartData>() {
+            @Override
+            public void onResponse(Call<CartData> call, Response<CartData> response) {
+
+                  //cartDatas.addAll(response.body().getData());
+                cart = response.body().getData();
+                   /*for (int i = 0; i < carts.size();i++){
+                     //  cartDatas.add(carts.get(i));
+                       Toast.makeText(getContext(),carts.get(i)+"dddf",Toast.LENGTH_SHORT).show();
+                   }*/
+                 //  Toast.makeText(getContext(),"获取数据成功",Toast.LENGTH_SHORT).show();
+              // Toast.makeText(getContext(),""+cart.get(0).getStorename(),Toast.LENGTH_LONG).show();
+                   mCartReCycleViewAdapter.setCartDatas(cart);
+
+            }
+
+            @Override
+            public void onFailure(Call<CartData> call, Throwable t) {
+                Toast.makeText(getContext(),"获取数据失败",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /*private List<List<CartData.DataBean>> getCartList(List<CartData.DataBean> cartlist){
+        lists = new ArrayList<List<CartData.DataBean>>();
+        List<CartData.DataBean> list = new ArrayList<CartData.DataBean>();
+        for (int i = 0; i < cartlist.size() ; i++) {
+            CartData.DataBean cart = cartlist.get(i);
+            list.add(cart);
+            lists.add(list);
+        }
+        return lists;
+    }*/
 }
