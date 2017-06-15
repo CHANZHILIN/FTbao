324361eb140483535db07a942cbe8dc306a87bae
jiabao <1194899285@qq.com>
2017-06-15 10:06:52 +0800

    自动登录

--- app/src/main/java/com/example/jinbiao/ftbao/activity/LoginActivity.java
+++ app/src/main/java/com/example/jinbiao/ftbao/activity/LoginActivity.java
@@ -1,18 +1,25 @@
 package com.example.jinbiao.ftbao.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.jinbiao.ftbao.MainActivity;
 import com.example.jinbiao.ftbao.R;
+import com.example.jinbiao.ftbao.utils.SharedHelper;
+import com.example.jinbiao.ftbao.utils.ToastUtils;
 
 import java.io.IOException;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,25 +47,53 @@ public class LoginActivity extends AppCompatActivity {
     TextView tvFindpassword;
     @BindView(R.id.toolbar_login)
     Toolbar toolbarLogin;
+    @BindView(R.id.cb_remember)
+    CheckBox cbRemember;
+    @BindView(R.id.cb_autologin)
+    CheckBox cbAutologin;
 
     private OkHttpClient client;
 
+
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
+        sp = getSharedPreferences("userinfo", Context.MODE_PRIVATE);
+        editor = sp.edit();
+        Long logintime = sp.getLong("logintime", 0);
+        Long currenttime = System.currentTimeMillis();
+        int lasttime = 3 * 24 * 60 * 60 * 1000;
+        if (!(currenttime - logintime > lasttime)) {
+            if (sp.getBoolean("autologin", false)) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                LoginActivity.this.finish();
+            }
+        }
         setContentView(R.layout.activity_login);
+
+
         ButterKnife.bind(this);
+        if (cbAutologin.isChecked()) {
+            startActivity(new Intent().setClass(this, MainActivity.class));
+        }
         setSupportActionBar(toolbarLogin);
     }
 
-    @OnClick({R.id.et_account, R.id.et_password, R.id.btn_login, R.id.tv_resister, R.id.tv_findpassword})
+    @OnClick({R.id.btn_login, R.id.tv_resister, R.id.tv_findpassword})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.et_account:
-                break;
-            case R.id.et_password:
-                break;
+
             case R.id.btn_login:
+                String username = etAccount.getText().toString();
+                String password = etPassword.getText().toString();
+                // postRequest(username,password);
+                //System.out.println(new Date());
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 break;
             case R.id.tv_resister:
@@ -87,11 +122,19 @@ public class LoginActivity extends AppCompatActivity {
         call.enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-
+                ToastUtils.toastShort(getApplicationContext(), "请填写正确的信息！");
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
+
+                String key = response.body().string();
+
+                String username = etAccount.getText().toString();
+
+                if (cbAutologin.isChecked()) {
+                    SharedHelper.saveInfo(username, key);
+                }
 
             }
         });
